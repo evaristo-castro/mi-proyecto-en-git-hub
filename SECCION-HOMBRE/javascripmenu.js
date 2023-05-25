652894bd56c3ee1666af2f2d318ebe489f5cd08b
EVARISTO <evariste96@hotmail.com>
2023-05-24 22:28:23 -0500

    js ocultar menu

---
+++ SECCION-HOMBRE/javascripmenu.js
@@ -0,0 +1,7 @@
+var toggleButton = document.getElementById('toggleButton');
+var menu = document.getElementById('menu');
+
+toggleButton.addEventListener('click', function(){
+
+    menu.classList.toggle('show');
+});
