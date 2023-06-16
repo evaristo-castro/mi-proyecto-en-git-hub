1bbf26260dadffe2954e995e68ff0053de6e7645
EVARISTO <evariste96@hotmail.com>
2023-06-15 19:21:24 -0500

    buscador con enter

--- SECCION-HOMBRE/arreglo.js
+++ SECCION-HOMBRE/arreglo.js
@@ -1,29 +1,29 @@
 // ARREGLO
 const infoProductos = [
         {
-            titulo: 'Rolex',
-            imagen: '/Image/hombres/reloj/uno.PNG',
+            titulo: 'Invicta',
+            imagen: '/Image/hombres/reloj/invicta.png',
             costo:'400000',
-            boton: 'button',
+            boton: 'Ver mas',
             cantidad: 5
         },
         {
-            titulo: 'casio',
-            imagen: '/Image/hombres/reloj/dos.PNG',
+            titulo: 'victorino',
+            imagen: '/Image/hombres/reloj/victorino.PNG',
             costo: '$3.000.000',
             boton:'ver mas',
             cantidad: 1
         },
         {
-            titulo: 'Monday',
-            imagen: '/Image/hombres/reloj/tres.PNG',
+            titulo: 'Tissot',
+            imagen: '/Image/hombres/reloj/tissot.PNG',
             costo: '$2.000.000',
             boton:'ver mas',
             cantidad: 1
         },
         {
-            titulo: 'QQ',
-            imagen: '/Image/hombres/reloj/cuatro.PNG',
+            titulo: 'Tissop',
+            imagen: '/Image/hombres/reloj/tissot2.PNG',
             costo: '$3.000.000',
             boton:'ver mas',
             cantidad: 1
@@ -39,6 +39,7 @@ infoProductos.forEach(function(producto)
         {
         console.log(producto);
         //crear los elementos
+        
         var tarjeta = document.createElement('div');
         var titulo = document.createElement('h2');
         var imagen = document.createElement('img');
@@ -47,28 +48,42 @@ infoProductos.forEach(function(producto)
         var cantidad = document.createElement('p');
 
         var tarjetaHeader = document.createElement('div');
+        var tarjetabody = document.createElement('div');
+        var tarjetafooter = document.createElement('div');
+
 
         // Asignar una clase al elemento
-
+       
         tarjeta.className = 'card bg-body text-center text-black w-25';
         tarjetaHeader.className = 'card-header';
+        tarjetabody.className = 'card-body';
+        tarjetafooter.className ='card-footer';
+
         titulo.className ='card-title';
         imagen.className = 'clase-img';
         costo.className = 'clase-costo';
+        boton.className='btn btn-outline-primary';
         //asignar valores a los elementos
         titulo.textContent = producto.titulo;
         imagen.src=producto.imagen;
         costo.textContent=producto.costo;
-        boton.boton =producto.boton;
+        boton.textContent =producto.boton;
         cantidad.textContent =producto.cantidad;
 
         // agregar elementos  a la tarjeta
         tarjetaHeader.appendChild(titulo);
+        tarjetabody.appendChild(imagen);
+        tarjetabody.appendChild(costo);
+        tarjetafooter.appendChild(boton);
+
         tarjeta.appendChild(tarjetaHeader);
-        tarjeta.appendChild(imagen);
-        tarjeta.appendChild(costo);
-        tarjeta.appendChild(boton);
-        tarjeta.appendChild(cantidad);
+        tarjeta.appendChild(tarjetabody);
+        tarjeta.appendChild(tarjetafooter);
+
+        
+         
+        
+        
 
 
         // agregar la tarjeta al contenedor
--- buscador.js
+++ buscador.js
@@ -1,27 +1,30 @@
 function fBuscador(){
-    //vincular input a la variable infoInput con ID, buscar, con .value se obtiene el valor
+            //vincular input a la variable infoInput con ID, buscar, con .value se obtiene el valor
+            
+            const infoInput = document.getElementById('buscar').value;
+          
+                    const datos = [
+                             { link: '/SECCION-HOMBRE/hombre.html', 
+                             titulo:'Hombres'
+                             },
+                             { link:'/CONTACTO/contacto.html', 
+                             titulo:'Contacto'
+                             }
+                    ]
+                // instancia de la clase y se envia los datos del arreglo
+                const buscador = new Buscador(datos);
+                // ejecutamos el metodo  y enviamos lo del input para comparar con filter
+                const resultados = buscador.buscar(infoInput);
+            
+              
+              
+            //mostrarInformacion(resultados);
+            redireccion(resultados); 
     
-    const infoInput = document.getElementById('buscar').value;
-   
-    const datos = [
-                    { link: '/SECCION-HOMBRE/hombre.html', 
-                      titulo:'Hombres'
-                    },
-                    { link:'/CONTACTO/contacto.html', 
-                    titulo:'Contacto'
-                    }
-    ]
-     // instancia de la clase y se envia los datos del arreglo
-     const buscador = new Buscador(datos);
-     // ejecutamos el metodo  y enviamos lo del input para comparar con filter
-     const resultados = buscador.buscar(infoInput)
-   
-      
-       //console.log(resultados);
-    mostrarInformacion(resultados);
 }
 
 // crear una funcion para mostrar en la web 
+
 
 function mostrarInformacion(informacion){
      // accedemos al div que muestra informacion 
@@ -33,7 +36,38 @@ function mostrarInformacion(informacion){
            link.href = objeto.link;
             
            divMostrarInfo.appendChild(link);
-
+           
      }
 
+      
 }
+function redireccion(paso){
+  for( let i = 0 ; i<paso.length; i++){
+    const objeto = paso[i];
+         
+    console.log(objeto);
+   window.location.href = objeto.link; 
+}
+   
+
+
+}
+// buscar con enter en input
+// Obtén una referencia al elemento input
+const input = document.querySelector('#buscar');
+
+// Agrega el event listener para el evento "keydown"
+input.addEventListener('keydown', function(event) {
+  // Verifica si la tecla presionada es "Enter"
+  if (event.keyCode === 13) {
+    // Realiza las acciones que deseas realizar
+    fBuscador();
+  }
+});
+
+
+
+
+
+
+
