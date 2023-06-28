ff72976cc5f986f718ebf90f3d576d9531d169fe
EVARISTO <evariste96@hotmail.com>
2023-06-27 19:51:44 -0500

    Validando Formulario

--- CONTACTO/mainClase.js
+++ CONTACTO/mainClase.js
@@ -12,44 +12,44 @@ constructor(nombres, apellido, email,mensaje, fecha,){
 
 validarNombres =() =>{
     
-  const miMensajeInvalido = document.getElementById('mensajeinvalido');
-    const nombresRegex = /^[a-zA-Z\s']+$/;
-  if(!this.name.match(nombresRegex)){
-    //alert("nombre invalido");
-    miMensajeInvalido.textContent = "Favor Introdusca bien su nombre";
+        const miMensajeInvalido = document.getElementById('mensajeinvalido');
+          const nombresRegex = /^[a-zA-Z\s']+$/;
+        if(!this.name.match(nombresRegex)){
+          //alert("nombre invalido");
+          miMensajeInvalido.textContent = "Favor Introdusca bien su nombre";
 
-  }
-   console.log('valido');
+        }
+        console.log('valido');
 
 }
 validarApellido =() =>{
-    const apellidoRegex = /^[a-zA-Z\s']+$/;
-  if(!this.apelli.match(apellidoRegex)){
-    alert("apellido invalido");
-    miMensajeInvalido.textContent = "Favor Introdusca bien su Apellido";
-  }
+        const apellidoRegex = /^[a-zA-Z\s']+$/;
+        if(!this.apelli.match(apellidoRegex)){
+        alert("apellido invalido");
+        miMensajeInvalido.textContent = "Favor Introdusca bien su Apellido";
+      }
 }
 
 validarCorreo =() =>{
-    const correoRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if(!this.correo.match(correoRegex)){
-    alert("correo invalido");
-    miMensajeInvalido.textContent = "Favor Introdusca bien su correo";
+        const correoRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if(!this.correo.match(correoRegex)){
+        alert("correo invalido");
+        miMensajeInvalido.textContent = "Favor Introdusca bien su correo";
   }
 }
 validarmensaje =() =>{
-    const mensajeRegex = /^[a-zA-Z0-9\s.,!?'"-]+$/;
-  if(!this.mensaj.match(mensajeRegex)){
-    alert("mensaje invalido");
-    miMensajeInvalido.textContent = "Favor Introdusca bien su Mesaje";
+        const mensajeRegex = /^[a-zA-Z0-9\s.,!?'"-]+$/;
+        if(!this.mensaj.match(mensajeRegex)){
+        alert("mensaje invalido");
+        miMensajeInvalido.textContent = "Favor Introdusca bien su Mesaje";
   }
 }
 validarfecha =(cadena) =>{
-    const fechaRegex = /^[a-zA-Z0-9\s.,!?'"-]+$/;
-  if(!this.fecha.match(fechaRegex)){
-    alert("Fecha invalida");
-    miMensajeInvalido.textContent = "Favor Introdusca bien su Fecha";
-  }
+        const fechaRegex = /^[a-zA-Z0-9\s.,!?'"-]+$/;
+        if(!this.fecha.match(fechaRegex)){
+        alert("Fecha invalida");
+        miMensajeInvalido.textContent = "Favor Introdusca bien su Fecha";
+      }
 }
 
 
